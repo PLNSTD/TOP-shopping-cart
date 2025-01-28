f6abd5ec94e75c087e4c0f477c220d3e40ee9aa6
Plidher <65611683+PLNSTD@users.noreply.github.com>
2025-01-28 17:49:50 -0500

    Fetched general products, improved styling

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,13 +1,14 @@
+import { PropTypes } from "prop-types";
 import Card from "./Card";
 
-const products = Array.from({ length: 30 }, (_, index) => ({
-  id: index + 1,
-  title: `Product ${index + 1}`,
-  price: (Math.random() * 50 + 10).toFixed(2), // Random price between 10 and 60
-  image: `https://via.placeholder.com/150?text=Product+${index + 1}`, // Dynamic image URL with product number
-}));
+// const staticProducts = Array.from({ length: 30 }, (_, index) => ({
+//   id: index + 1,
+//   title: `Product ${index + 1}`,
+//   price: (Math.random() * 50 + 10).toFixed(2), // Random price between 10 and 60
+//   image: `https://via.placeholder.com/150?text=Product+${index + 1}`, // Dynamic image URL with product number
+// }));
 
-const ProductList = () => {
+const ProductList = ({ products }) => {
   return (
     <div className="flex flex-wrap gap-x-2 w-full justify-center">
       {products.map((product) => (
@@ -24,6 +25,10 @@ const ProductList = () => {
       ))}
     </div>
   );
+};
+
+ProductList.propTypes = {
+  products: PropTypes.Array.isrequired,
 };
 
 export default ProductList;
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from "react";
 import ProductList from "../components/ProductList";
 
 const Shop = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    // Fetch products from the API
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Our Products</h1>
-      <ProductList />
+      <ProductList products={products} />
     </>
   );
 };
