cdb06bf1378727caac8abb46963e0f8c0b188641
Plidher <65611683+PLNSTD@users.noreply.github.com>
2025-01-27 19:30:47 -0500

    Add reusable Card component with prop validation

---
+++ src/components/Card.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+
+const Card = ({ title, image, price, description, onAddToCart }) => {
+  return (
+    <div>
+      <img src={image} alt={title} />
+      <div>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <div>
+          <span>{price}</span>
+          <button onClick={onAddToCart}>Add to Cart</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string,
+  onAddToCart: PropTypes.func.isRequired,
+};
+
+export default Card;
